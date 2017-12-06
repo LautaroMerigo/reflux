03726a0fd9831c0a26ebdffc72e4a725034784a8
Lautaro Merigo <lautaromerigo@gmail.com>
2017-12-06 15:06:53 -0300

    Third commit

--- server.js
+++ server.js
@@ -6,6 +6,36 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 const open = require('open');
 
+
+const express = require('express');
+const http = require('http');
+const engine = require('socket.io');
+
+const request = require('request');
+
+const port = 3000;
+const app = express();
+
+let server = http.createServer(app).listen(port, () => {
+  console.log("port listening in " + port);
+});
+
+const io = engine.listen(server);
+
+io.on('connection', (socket) => {
+
+  request('https://randomuser.me/api/', (err, response, body) => {
+    io.emit('people', body);
+  });
+
+  socket.on('ask', (ask) => {
+    request('https://randomuser.me/api/', (err, response, body) => {
+      io.emit('people', body);
+    });
+  });
+
+});
+
 /**
  * Flag indicating whether webpack compiled for the first time.
  * @type {boolean}
@@ -14,12 +44,17 @@ let isInitialCompilation = true;
 
 const compiler = webpack(config);
 
+
+
+
 new WebpackDevServer(compiler, config.devServer)
 .listen(config.port, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
   console.log('Listening at localhost:' + config.port);
+  console.log('Opening your system browser');
+  open('http://localhost:' + config.port + '/webpack-dev-server');
 });
 
 compiler.plugin('done', () => {
